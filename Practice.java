22cfaca24a47e745d47a2f23dfc5db3d61f8c297
jagadeeshthiru <jagaddeeshthirumareddy123@gmail.com>
2024-11-06 11:37:39 +0530

    Commit by sam

---
+++ Practice.java
@@ -0,0 +1,20 @@
+class  Practise
+{
+	public static void main(String[] args) 
+	{
+		int num =40;
+		
+		if (num%3==0)
+		{
+		System.out.println("Fizz");
+		}
+		else if(num%5==0)
+		{
+			System.out.println("Buzz");
+		}
+		else
+		{
+             System.out.println("Invalid");
+		}
+	}
+}
---
+++ code.java
@@ -0,0 +1,11 @@
+
+<html>
+     <head>
+     </head>
+     <body>
+      <i>Iam Jagadeesh </i>
+     <b> Iam from Vizianagaram </b>
+<u> Iam in Hderabad</u>
+       </body>
+
+</html>
